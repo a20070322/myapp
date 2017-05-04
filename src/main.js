c17ea2ca5249efed5e913f3bd018a2175b440fe8
a20070322 <1031839775@qq.com>
2017-05-04 21:07:14 +0800

    构建项目

---
+++ src/main.js
@@ -0,0 +1,9 @@
+Vue.config.productionTip = false
+require('./styles/app.scss')
+import Vue from 'vue'
+import index from './scripts/components/index.vue'
+new Vue({
+  el: '#app',
+  components: { index },
+  template: '<index />'
+})
