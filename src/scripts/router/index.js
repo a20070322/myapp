b407ef3be03b208a22b84494a88e01eac5952718
unknown <244254588@qq.com>
2017-05-10 13:53:07 +0800

    you1

--- src/scripts/router/index.js
+++ src/scripts/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/scripts/components/Index'
-
-
-
 import Home from '@/scripts/components/home'
 import Classify from '@/scripts/components/classify'
 import Cart from '@/scripts/components/cart'
 import Me from '@/scripts/components/me'
+import Search from '@/scripts/components/home/search/search'
+import Location from '@/scripts/components/home/location/location'
 
 Vue.use(Router)
+
 export default new Router({
   routes: [
     {
@@ -21,7 +21,7 @@ export default new Router({
         {
           //首页
           path: '/home',
-          component: Home
+          component: Home,
         },
         {
           //分类
@@ -37,9 +37,22 @@ export default new Router({
           //我的
           path:'/me',
           component:Me
-        }
+        },
+
       ]
     },
+    {
+      path: '/search',
+      component: Search
+    },
+    {
+      path: '/location',
+      component: Location
+    },
+    {
+      path: '/yue',
+      component: Yue
+    }
     // {
     //   // 详情路由
     //   path:'/details/:id'
