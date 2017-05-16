42332329470f4539c5e0b15be1ef60c0c746817e
liyuxin <477930154@qq.com>
2017-05-16 16:56:16 +0800

    5.16

--- src/scripts/components/cart/index.vue
+++ src/scripts/components/cart/index.vue
@@ -1,5 +1,5 @@
 <template lang="html">
- <!--  <div class="cart" v-if>
+  <div class="cart" v-if>
   	<header>
 		购物车
   	</header>
@@ -12,26 +12,10 @@
      
   	</section>
   	
-  </div> -->
+  </div>
 
 
-   <div class="carts" v-else>
-    <header>
-    购物车
-    <span class="c_span">编辑</span>
-    </header>
-    <section>
-     <ul class="c_ul">
-        <li>
-          <div class="L"><input type="radio" class="c_dan"></div>
-          <div class="C"><img src="" alt="" class="imgs"></div>
-          <div class="R">
-            <span>跨境</span>
-            <p>新西兰和牛</p>
-            规格<span></span>
-            <p>生冻</p>
-            ￥<p>188</p>
-          </div>
+  
 
          
 
